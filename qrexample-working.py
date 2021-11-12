e99dbc00a44b777d3e7f0c71f88718d998cbc55a
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-11-11 23:42:38 -0800

    comment

--- qrexample-working.py
+++ qrexample-working.py
@@ -1,9 +1,8 @@
-import pyqrcode
 from PIL import Image
 import qrcode
 from qrcode.image.styledpil import StyledPilImage
-from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 from qrcode.image.styles.colormasks import RadialGradiantColorMask
+# from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -22,13 +21,13 @@ qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
 qr.add_data(lnurl)
 
 img_2 = qr.make_image(image_factory=StyledPilImage, color_mask=RadialGradiantColorMask(back_color=GREY,
-                                                                                       center_color=WHITE,
+                                                                                       center_color=YELLOW,
                                                                                        edge_color=RED))
 img_2.save('images/lnurl.png')
 im = Image.open('images/lnurl.png')
 im = im.convert("RGBA")
 
-logo = Image.open('images/red_logo.png')
+logo = Image.open('images/grey_logo.png')
 #box = (220,220,320,320)
 box = (215,215,315,315)
 im.crop(box)
