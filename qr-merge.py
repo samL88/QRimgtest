9afd5b36785e8753b360ecc38d77133fdffae741
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-12-03 01:28:06 -0800

    edit fill color

--- qr-merge.py
+++ qr-merge.py
@@ -1,8 +1,8 @@
 #from PIL import Image
 import qrcode
 import qrcode.image.svg
-#from qrcode.image.styledpil import StyledPilImage
-#from qrcode.image.styles.colormasks import RadialGradiantColorMask
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.colormasks import RadialGradiantColorMask
 
 RED = (255, 0, 0)
 DEEP_RED = (170, 0, 0)
@@ -10,14 +10,29 @@ WHITE = (255, 255, 255)
 
 lnurl = "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AKXUATJD3CZ7CTSDYHHVVF0D3H82UNV9UUNWVCE4EM6P"
 
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
 # 30% or greater error conversion
-qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+#qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
 qr.add_data(lnurl)
+qr.make(fit=True)
 
 factory = qrcode.image.svg.SvgPathImage
+
+# QR_PATH_STYLE = {'fill': '#000000', 'fill-opacity': '1',
+#                     'fill-rule': 'nonzero', 'stroke': 'none'}
+
+# update fill color to red
+factory.QR_PATH_STYLE['fill'] = '#ae0909'
+print(factory.QR_PATH_STYLE['fill'])
+
 svg_img = qrcode.make(lnurl, image_factory=factory)
 svg_img.save('qrred.svg')
-
 
 #QRimg = qr.make_image(image_factory=StyledPilImage, 
 #                     color_mask=RadialGradiantColorMask(back_color=WHITE, 
