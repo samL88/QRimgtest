a5925df1688fd063d512248a20ecae9fbc042582
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-12-04 01:49:24 -0800

    consolidate update

--- rsvg-convert.sh
+++ rsvg-convert.sh
@@ -4,6 +4,6 @@
 # brew install librsvg
 # docs: https://www.systutorials.com/docs/linux/man/1-rsvg-convert/
 
-rsvg-convert output.svg > output.png
+rsvg-convert output.svg -o output.png -w 600
 
 #-b white -w 1000 -h 1000
--- template_test.py
+++ template_test.py
@@ -8,13 +8,15 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.colormasks import RadialGradiantColorMask
 
 import pyqrcode
+import subprocess
 
+# QR_PATH_STYLE = {'fill': '#000000', 'fill-opacity': '1',
+#                     'fill-rule': 'nonzero', 'stroke': 'none'}
+# update fill color to red, # gold yellow #C3B66C
 
 RED = (255, 0, 0)
 DEEP_RED = (170, 0, 0)  # '#ae0909'
 WHITE = (255, 255, 255)
-L_YELLOW = (195,182,108) # #C3B66C
-D_YELLOW = (142, 116, 56) # #8E7438
 
 lnurl = "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AKXUATJD3CZ7CTSDYHHVVF0D3H82UNV9UUNWVCE4EM6P"
 
@@ -23,40 +25,12 @@ lnurl_file = "images/lnurl.png"
 pyqr = pyqrcode.create(lnurl)
 pyqr.png(lnurl_file, scale=3, module_color=[255,255,255,255], background=[170, 0, 0])
 
-
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=10,
     border=0.5,
 )
-
-# 30% or greater error conversion
-#qr.add_data(lnurl)
-#qr.make(fit=True)
-
-#factory = qrcode.image.svg.SvgPathImage
-
-# QR_PATH_STYLE = {'fill': '#000000', 'fill-opacity': '1',
-#                     'fill-rule': 'nonzero', 'stroke': 'none'}
-# update fill color to red, # gold yellow #C3B66C
-'''
-red = '#ae0909'
-yellow = '#C3B66C'
-dkyellow = '#8E7438'
-black = '#000000'
-white = '#ffffff'
-other = '#5b1110ff'
-
-factory.QR_PATH_STYLE['fill'] = '#ffffff'
-print(factory.QR_PATH_STYLE['fill'])
-
-factory.background = red
-print(factory.background)
-
-svg_img = qrcode.make(lnurl, image_factory=factory)
-svg_img.save('qrcolor.svg')
-'''
 
 with open('templates/inlet_tiger_cut.svg', 'r') as f:
     templ = f.read()
@@ -65,11 +39,18 @@ with open('templates/inlet_tiger_cut.svg', 'r') as f:
 qr_code = "\"" + "images/lnurl.png" + "\""
 idnumber = "f7dfwer7a8cd43aabsdfs"
 expires = "2022-03-15"
-sats = "2000"
+sats = "2500"
 
 tm = Template(templ)
 msg = tm.render(qrcode=qr_code, idnumber=idnumber, expires=expires, sats=sats)
 
-with open('output.svg', 'w') as f:
+output_svg = 'output.svg'
+with open(output_svg, 'w') as f:
     res = f.write(msg)
     f.close()
+
+output_png = 'output.png'
+subprocess.run(['rsvg-convert', output_svg, '-o', output_png, '-w' , '600'], cwd=".")
+
+# rsvg-convert output.svg -o output.png -w 600
+subprocess.run(['open', output_png])
