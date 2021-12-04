a16326d20550a621e5c836f27a562aa87f626663
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-12-04 01:55:15 -0800

    make method

--- template_test.py
+++ template_test.py
@@ -20,36 +20,44 @@ WHITE = (255, 255, 255)
 
 lnurl = "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AKXUATJD3CZ7CTSDYHHVVF0D3H82UNV9UUNWVCE4EM6P"
 
-# this works
-lnurl_file = "images/lnurl.png"
-pyqr = pyqrcode.create(lnurl)
-pyqr.png(lnurl_file, scale=3, module_color=[255,255,255,255], background=[170, 0, 0])
-
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=0.5,
-)
-
-with open('templates/inlet_tiger_cut.svg', 'r') as f:
-    templ = f.read()
-
-qr_code = "\"" + lnurl_file + "\""
+lnurl_file = "images/lnurl.png" # make this id number based for uniqueness
+output_svg = 'output.svg'
+output_png = 'output.png'
 idnumber = "f7dfwer7a8cd43aabsdfs"
 expires = "2022-03-15"
 sats = "2500"
 
-tm = Template(templ)
-msg = tm.render(qrcode=qr_code, idnumber=idnumber, expires=expires, sats=sats)
 
-output_svg = 'output.svg'
-with open(output_svg, 'w') as f:
-    res = f.write(msg)
-    f.close()
+def create_laisee_qrcode(lnurl: str, idnumber: str, expires: str, sats: str):
+    # todo: check if lnurl is valid
+    pyqr = pyqrcode.create(lnurl)
+    pyqr.png(lnurl_file, scale=3, module_color=[255,255,255,255], background=[170, 0, 0])
 
-output_png = 'output.png'
-subprocess.run(['rsvg-convert', output_svg, '-o', output_png, '-w' , '600'], cwd=".")
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=0.5,
+    )
 
-# rsvg-convert output.svg -o output.png -w 600
-subprocess.run(['open', output_png])
+    with open('templates/inlet_tiger_cut.svg', 'r') as f:
+        templ = f.read()
+
+    qr_code = "\"" + lnurl_file + "\""
+    tm = Template(templ)
+    msg = tm.render(qrcode=qr_code, idnumber=idnumber, expires=expires, sats=sats)
+
+    with open(output_svg, 'w') as f:
+        res = f.write(msg)
+        f.close()
+
+    subprocess.run(['rsvg-convert', output_svg, '-o', output_png, '-w' , '600'], cwd=".")
+    return output_png
+
+
+if __name__ == "__main__":
+
+    output_png = create_laisee_qrcode(lnurl, idnumber, expires, sats)
+
+    # view file created
+    subprocess.run(['open', output_png])
